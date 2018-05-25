15c263cb43ad3f04b3d76ab3dce51c74e091c11d
Osvius <osvius@yandex.ru>
2018-05-25 14:37:11 +0300

    add possibility to change password with separate link

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  prepend_before_action :authenticate_scope!, only: [:edit, :update, :destroy, :change_password]
 
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:attribute], except: [:password])
@@ -7,4 +8,41 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
+
+  def change_password
+    render :change_password
+  end
+
+  def update_password
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+
+    resource_updated = update_user_password(resource, password_update_params)
+    yield resource if block_given?
+    if resource_updated
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+                        :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      bypass_sign_in resource, scope: resource_name
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      render :change_password
+    end
+  end
+
+  protected
+
+  def password_update_params
+    devise_parameter_sanitizer.sanitize(:account_update)
+  end
+
+  def update_user_password(resource, params)
+    resource.update_with_password(params)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: "users/registrations"}
+  devise_scope :user do
+    get 'users/password/change', to: 'users/registrations#change_password', as: "change_user_password"
+    put 'users/password/update', to: 'users/registrations#update_password', as: "update_user_password"
+    get 'users/password/update', to: 'users/registrations#update_password'
+  end
+  devise_for :users, controllers: {registrations: "users/registrations", passwords: "users/passwords"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome/index'
 
